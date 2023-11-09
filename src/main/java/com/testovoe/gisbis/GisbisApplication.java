81b4334b214fd53dff652a2ca3a563c925398aaa
awesomemax <cvb.cvb.2017.cvb@gmail.com>
2023-11-09 21:35:22 +0300

    init commit

---
+++ src/main/java/com/testovoe/gisbis/GisbisApplication.java
@@ -0,0 +1,13 @@
+package com.testovoe.gisbis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GisbisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GisbisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testovoe/gisbis/GisbisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testovoe.gisbis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisbisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
